9b608b8f5b012f4abd93fea8a78015fb9cb13c2a
akcyp <pycka.bartosz@gmail.com>
2022-12-28 15:02:01 +0100

    Fix invalid websocket protocol

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,6 +2,6 @@ export const ENDPOINTS = {
   API: API_URL === '/' ? '/api' : `${API_SECURE ? 'https' : 'http'}://${API_URL}/api`,
   WS:
     API_URL === '/'
-      ? `${location.protocol === 'https' ? 'wss' : 'ws'}://${location.host}/ws`
+      ? `${location.protocol === 'https:' ? 'wss' : 'ws'}://${location.host}/ws`
       : `${API_SECURE ? 'wss' : 'ws'}://${API_URL}/ws`,
 };
